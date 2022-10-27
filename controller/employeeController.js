ac1d853237e07c64267f4b3ffd7775abb5c40ac8
unknown <thongnguyennqt@gmail.com>
2022-10-28 06:08:29 +0700

    6pm

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Employee = mongoose.model('Employee');
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/ad", (req, res) => {
     res.render("employee/addOrEdit", {
         viewTitle: "Insert Employee"
     })
